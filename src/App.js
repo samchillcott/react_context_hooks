48c3a135098bc3075243b99d3bf6a61153c9cb4e
Sam Chillcott <sam@samchillcott.com>
2020-06-26 10:54:29 +0100

    Nav + import to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import MovieList from './MovieList'; 
+import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <MovieList />
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+	return (
+		<div>
+			<h3>Sam Chillcott</h3>
+            <p>List Of Movies: </p>
+		</div>
+	);
+};
+
+export default Nav;
