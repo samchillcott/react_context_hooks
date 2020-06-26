12ae1c85ecf86fdba2d190f3f7402fa8e279e96b
Sam Chillcott <sam@samchillcott.com>
2020-06-26 12:24:35 +0100

    nav display movies

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
+import {MovieContext} from './MovieContext';
 
 const Nav = () => {
-	return (
+    const [movies, setMovies] = useContext(MovieContext);
+    return (
 		<div>
 			<h3>Sam Chillcott</h3>
-			<p>List Of Movies: </p>
+			<p>List Of Movies: {movies.length}</p>
 		</div>
 	);
 };
