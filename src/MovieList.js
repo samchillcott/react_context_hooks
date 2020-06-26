1d5ce8cdc9721f079e0bca7de70b9638684e2cb8
Sam Chillcott <sam@samchillcott.com>
2020-06-26 09:43:20 +0100

    compnonent connection

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 
-function movielist() {
+function MovieList() {
 	const [movies, setMovies] = useState([
 		{
 			name: "Teen Wolf",
 			price: "$10",
-			id: "12345",
+			id: 12345,
 		},
 		{
 			name: "Die Hard",
 			price: "$10",
-			id: "12456",
+			id: 12456,
 		},
 		{
 			name: "High School High",
 			price: "$10",
-			id: "23456",
+			id: 23456,
 		},
 	]);
 
 	return <div></div>;
 }
 
-export default movielist;
+export default MovieList;
