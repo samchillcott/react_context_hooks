b15d006ba80ffaead9dd0d68414279d995a299b2
Sam Chillcott <sam@samchillcott.com>
2020-06-27 10:17:25 +0100

    name and price update functions

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
 
 const AddMovie = () => {
+	const [name, setName] = useState("");
+	const [price, setPrice] = useState("");
+
+	const updateName = (e) => {
+        // take teh value of input 1 and set it to state
+        setName(e.target.value);
+    };
+
+    const updatePrice = (e) => {
+        // take teh value of input 2 and set it to state
+        setPrice(e.target.value);
+    };
+
+    // onSubmit
+
+    
+
 	return (
 		<form>
-			<input type="text" name="name" />
-			<input type="text" name="price" />
+			<input type="text" name="name" value={name} onChange={updateName}/>
+			<input type="text" name="price" value={price} onChange={updatePrice}/>
 			<button>Submit</button>
 		</form>
 	);
 };
+
+export default AddMovie;
