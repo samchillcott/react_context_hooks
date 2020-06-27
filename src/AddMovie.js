eed4c4951c4e1e8c3402e526ba48f48914eb25d7
Sam Chillcott <sam@samchillcott.com>
2020-06-27 10:44:57 +0100

    addmovie clear on submit

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,27 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { MovieContext } from "./MovieContext";
 
 const AddMovie = () => {
 	const [name, setName] = useState("");
 	const [price, setPrice] = useState("");
+	const [movies, setMovies] = useContext(MovieContext);
 
 	const updateName = (e) => {
-        // take teh value of input 1 and set it to state
-        setName(e.target.value);
-    };
+		// take teh value of input 1 and set it to state
+		setName(e.target.value);
+	};
 
-    const updatePrice = (e) => {
-        // take teh value of input 2 and set it to state
-        setPrice(e.target.value);
-    };
+	const updatePrice = (e) => {
+		// take teh value of input 2 and set it to state
+		setPrice(e.target.value);
+	};
 
-    // onSubmit
+	// onSubmit = take teh name and price values and add it to the movie state as another entry/object
 
-    
+	const addMovie = (e) => {
+		e.preventDefault();
+		setMovies((prevMovies) => [...prevMovies, { name: name, price: price }]);
+		setName("");
+		setPrice("");
+	};
 
 	return (
-		<form>
-			<input type="text" name="name" value={name} onChange={updateName}/>
-			<input type="text" name="price" value={price} onChange={updatePrice}/>
+		<form onSubmit={addMovie}>
+			<input
+				type="text"
+				name="name"
+				placeholder="Movie name"
+				value={name}
+				onChange={updateName}
+			/>
+			<input
+				type="text"
+				name="price"
+				placeholder="Price"
+				value={price}
+				onChange={updatePrice}
+			/>
 			<button>Submit</button>
 		</form>
 	);
